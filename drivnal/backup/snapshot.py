54bf1b4f5af484604084dd18d08b8006071ed2e1
Zachary Huff <zach.huff.386@gmail.com>
2013-08-10 20:39:27 -0400

    Fix snapshot log file name

--- drivnal/backup/snapshot.py
+++ drivnal/backup/snapshot.py
@@ -23,7 +23,8 @@ class Snapshot(Bucket):
             self.state = COMPLETE
         self.volume = volume
         self.path = os.path.join(self.volume.path, id)
-        self.log_path = os.path.join(self.volume.path, str(self.id)) + '.log'
+        self.log_path = os.path.join(
+            self.volume.path, 'snapshot_%s.log' % self.id)
         self.parse_log_file()
 
     def __getattr__(self, name):
