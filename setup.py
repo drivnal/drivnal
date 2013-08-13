c7741449de562f14bb025ef2c3ac2cc70043e549
Zachary Huff <zach.huff.386@gmail.com>
2013-08-13 13:56:51 -0400

    Update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='drivnal',
-    version='0.1.0',
+    version='0.1.1',
     description='Linux backup utility with web interface',
     author='Zachary Huff',
     author_email='zach.huff.386@gmail.com',
