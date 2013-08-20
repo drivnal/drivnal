52cc04af5c3b60508917a190f39679c6509acde4
Zachary Huff <zach.huff.386@gmail.com>
2013-08-20 12:08:50 -0400

    Seperate server setup functions for debugging

--- drivnal/server.py
+++ drivnal/server.py
@@ -1,11 +1,9 @@
 import os
-import flask
-import cherrypy.wsgiserver
 import logging
 import signal
+import time
 from constants import *
 from database import Database
-from werkzeug import SharedDataMiddleware
 
 logger = None
 
@@ -15,17 +13,21 @@ class Server:
         self.conf_path = None
         self.app = None
         self.app_db = None
+        self.scheduler = None
 
     def _setup_app(self):
+        import flask
         self.app = flask.Flask(APP_NAME)
 
         global logger
         logger = self.app.logger
 
+    def _setup_conf(self):
         from config import Config
         self.config = Config(self.conf_path)
         self.config.read()
 
+    def _setup_log(self):
         if self.config.log_debug == 'true':
             self.log_level = logging.DEBUG
         else:
@@ -36,6 +38,10 @@ class Server:
         else:
             self.log_handler = logging.StreamHandler()
 
+        global logger
+        if not logger:
+            logger = logging.getLogger(APP_NAME)
+
         logger.setLevel(self.log_level)
         self.log_handler.setLevel(self.log_level)
 
@@ -45,12 +51,16 @@ class Server:
 
         logger.addHandler(self.log_handler)
 
+    def _setup_db(self):
         self.app_db = Database(self.config.db_path or DEFAULT_DB_PATH)
 
+    def _setup_handlers(self):
         import handlers
 
-    def _setup_static(self):
+    def _setup_static_handler(self):
         www_path = self.config.www_path or DEFAULT_WWW_PATH
+
+        from werkzeug import SharedDataMiddleware
 
         self.app.wsgi_app = SharedDataMiddleware(self.app.wsgi_app, {
             '/': os.path.normpath(www_path),
@@ -61,12 +71,16 @@ class Server:
             with open(os.path.join(www_path, 'index.html'), 'r') as fd:
                 return fd.read()
 
-    def _start_debug(self):
-        scheduler = None
-        if self.config.scheduler != 'false':
-            from scheduler import Scheduler
-            scheduler = Scheduler()
-            scheduler.start()
+    def _setup_all(self):
+        self._setup_app()
+        self._setup_conf()
+        self._setup_log()
+        self._setup_db()
+        self._setup_handlers()
+        self._setup_static_handler()
+
+    def _run_wsgi_debug(self):
+        logger.info('Starting debug server...')
 
         # App.run server uses werkzeug logger
         werkzeug_logger = logging.getLogger('werkzeug')
@@ -78,16 +92,13 @@ class Server:
                 port=int(self.config.port), threaded=True)
         finally:
             signal.signal(signal.SIGINT, signal.SIG_IGN)
+            self._stop_scheduler()
             logger.info('Stopping server...')
-            if scheduler:
-                scheduler.stop()
 
-    def _start(self):
-        scheduler = None
-        if self.config.scheduler != 'false':
-            from scheduler import Scheduler
-            scheduler = Scheduler()
-            scheduler.start()
+    def _run_wsgi(self):
+        import cherrypy.wsgiserver
+
+        logger.info('Starting server...')
 
         server = cherrypy.wsgiserver.CherryPyWSGIServer(
             (self.config.bind_addr, int(self.config.port)), self.app)
@@ -95,18 +106,54 @@ class Server:
             server.start()
         except (KeyboardInterrupt, SystemExit), exc:
             signal.signal(signal.SIGINT, signal.SIG_IGN)
+            self._stop_scheduler()
             logger.info('Stopping server...')
-            if scheduler:
-                scheduler.stop()
             server.stop()
 
-    def start(self):
-        self._setup_app()
-        self._setup_static()
-
-        logger.info('Starting server...')
-
+    def _run_server(self):
         if self.config.debug == 'true':
-            self._start_debug()
+            self._run_wsgi_debug()
         else:
-            self._start()
+            self._run_wsgi()
+
+    def _start_scheduler(self):
+        logger.info('Starting scheduler...')
+
+        if self.config.scheduler != 'false':
+            from scheduler import Scheduler
+            self.scheduler = Scheduler()
+            self.scheduler.start()
+
+    def _stop_scheduler(self):
+        logger.info('Stopping scheduler...')
+
+        if self.scheduler:
+            self.scheduler.stop()
+
+    def run_scheduler(self):
+        self._setup_conf()
+        self._setup_log()
+        self._setup_db()
+
+        self._start_scheduler()
+
+        try:
+            while True:
+                time.sleep(1)
+        except:
+            pass
+        finally:
+            self._stop_scheduler()
+
+    def run_server(self):
+        self._setup_all()
+        self._run_server()
+
+    def run_all(self):
+        self._setup_all()
+        self._start_scheduler()
+        self._run_server()
+
+    def start(self):
+        # TODO Replace usage of start with run_all
+        return self.run_all()
