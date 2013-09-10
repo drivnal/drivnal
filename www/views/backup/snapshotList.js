e358829e6acd096a56d361b49c7e60df254af37f
Zachary Huff <zach.huff.386@gmail.com>
2013-09-09 21:09:03 -0400

    Add log viewer for snapshots

--- www/views/backup/snapshotList.js
+++ www/views/backup/snapshotList.js
@@ -5,9 +5,11 @@ define([
   'collections/backup/snapshot',
   'views/backup/snapshot',
   'views/backup/itemList',
+  'views/backup/textLog',
+  'models/backup/textLog',
   'text!templates/backup/snapshotList.html'
 ], function($, _, Backbone, SnapshotCollection, SnapshotView, ItemListView,
-    snapshotListTemplate) {
+    TextLogView, TextLogModel, snapshotListTemplate) {
   'use strict';
   var SnapshotListView = ItemListView.extend({
     template: _.template(snapshotListTemplate),
@@ -16,7 +18,27 @@ define([
     title: 'Snapshots',
     removeTitle: 'Delete Selected Snapshots',
     selectable: true,
-    defaultOpen: true
+    defaultOpen: true,
+    onLogView: function(snapshot) {
+      var model = new TextLogModel({
+        id: snapshot,
+        volume: this.collection.getVolume(),
+        type: 'snapshot',
+        title: 'Snapshot Log',
+        subText: window.formatTime(snapshot)
+      });
+
+      this.textView = new TextLogView({
+        model: model
+      });
+
+      this.textView.model.fetch({
+        success: function() {
+          this.$el.parent().parent().prepend(this.textView.render().el);
+          this.updateSize();
+        }.bind(this)
+      });
+    }
   });
 
   return SnapshotListView;
