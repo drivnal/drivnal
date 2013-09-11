ab43572e3fc1f1e1e1a1a703377c8921c212bcc3
Zachary Huff <zach.huff.386@gmail.com>
2013-09-11 16:24:09 -0400

    Add log viewer to task list view

--- www/views/backup/taskList.js
+++ www/views/backup/taskList.js
@@ -4,14 +4,37 @@ define([
   'backbone',
   'collections/backup/task',
   'views/backup/task',
-  'views/backup/itemList'
-], function($, _, Backbone, TaskCollection, TaskView, ItemListView) {
+  'views/backup/itemList',
+  'views/backup/textLog',
+  'models/backup/textLog'
+], function($, _, Backbone, TaskCollection, TaskView, ItemListView,
+    TextLogView, TextLogModel) {
   'use strict';
   var TaskListView = ItemListView.extend({
     collectionClass: TaskCollection,
     viewClass: TaskView,
     title: 'Tasks',
     removeTitle: 'Remove Selected Tasks',
+    onLogView: function(taskId) {
+      var model = new TextLogModel({
+        id: taskId,
+        volume: this.collection.getVolume(),
+        type: 'task',
+        title: 'Task Log',
+        subText: taskId
+      });
+
+      this.textView = new TextLogView({
+        model: model
+      });
+
+      this.textView.model.fetch({
+        success: function() {
+          this.$el.parent().parent().prepend(this.textView.render().el);
+          this.updateSize();
+        }.bind(this)
+      });
+    }
   });
 
   return TaskListView;
