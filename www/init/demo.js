d864ea94c37f6517d00ca3dbf1aff7f77a60a455
Zachary Huff <zach.huff.386@gmail.com>
2013-09-11 00:03:26 -0400

    Change urls for ace worker files

--- www/init/demo.js
+++ www/init/demo.js
@@ -3,12 +3,23 @@ define([
   'underscore',
   'backbone',
   'googleAnalytics',
+  'ace',
   'demo/ajax'
-], function($, _, Backbone, GoogleAnalytics, demoAjax) {
+], function($, _, Backbone, GoogleAnalytics, Ace, demoAjax) {
   'use strict';
   var initialize = function() {
     GoogleAnalytics.push(['_setAccount', 'UA-20492194-10']);
     //GoogleAnalytics.push(['_setDomainName', 'demo.drivnal.com']);
+
+    Ace.config.setModuleUrl('ace/mode/coffee_worker', 'js/worker-coffee.js');
+    Ace.config.setModuleUrl('ace/mode/css_worker', 'js/worker-css.js');
+    Ace.config.setModuleUrl('ace/mode/javascript_worker',
+      'js/worker-javascript.js');
+    Ace.config.setModuleUrl('ace/mode/json_worker', 'js/worker-json.js');
+    Ace.config.setModuleUrl('ace/mode/lua_worker', 'js/worker-lua.js');
+    Ace.config.setModuleUrl('ace/mode/php_worker', 'js/worker-php.js');
+    Ace.config.setModuleUrl('ace/mode/xquery_worker', 'js/worker-xquery.js');
+
     Backbone.ajax = demoAjax;
   };
 
--- www/init/production.js
+++ www/init/production.js
@@ -2,12 +2,22 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'googleAnalytics'
-], function($, _, Backbone, GoogleAnalytics) {
+  'googleAnalytics',
+  'ace'
+], function($, _, Backbone, GoogleAnalytics, Ace) {
   'use strict';
   var initialize = function() {
     GoogleAnalytics.push(['_setAccount', 'UA-20492194-11']);
     //GoogleAnalytics.push(['_setDomainName', 'production.drivnal.com']);
+
+    Ace.config.setModuleUrl('ace/mode/coffee_worker', 'js/worker-coffee.js');
+    Ace.config.setModuleUrl('ace/mode/css_worker', 'js/worker-css.js');
+    Ace.config.setModuleUrl('ace/mode/javascript_worker',
+      'js/worker-javascript.js');
+    Ace.config.setModuleUrl('ace/mode/json_worker', 'js/worker-json.js');
+    Ace.config.setModuleUrl('ace/mode/lua_worker', 'js/worker-lua.js');
+    Ace.config.setModuleUrl('ace/mode/php_worker', 'js/worker-php.js');
+    Ace.config.setModuleUrl('ace/mode/xquery_worker', 'js/worker-xquery.js');
   };
 
   return initialize;
