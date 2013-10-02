40dfbc2b8c29935a2c48fd7c25c47bf9a790880e
Zachary Huff <zach.huff.386@gmail.com>
2013-10-02 18:25:48 -0400

    Increase version

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Zachary Huff <zach.huff.386@gmail.com>
 
 pkgname=drivnal
-pkgver=0.1.21
+pkgver=0.1.22
 pkgrel=1
 pkgdesc="Linux backup utility with web interface"
 arch=("any")
--- drivnal/__init__.py
+++ drivnal/__init__.py
@@ -1,6 +1,6 @@
 from constants import *
 __title__ = APP_NAME
-__version__ = '0.1.21'
+__version__ = '0.1.22'
 __author__ = 'Zachary Huff'
 __license__ = 'AGPL'
 __copyright__ = 'Copyright 2013 Zachary Huff'
