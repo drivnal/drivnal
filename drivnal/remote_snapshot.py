1471f18fec128f70a823a50d2f77de1f97d414cc
Zachary Huff <zach.huff.386@gmail.com>
2013-11-12 16:51:35 -0500

    Remove code for ssh backups

--- drivnal/remote_snapshot.py
+++ drivnal/remote_snapshot.py
@@ -6,14 +6,10 @@ logger = logging.getLogger(APP_NAME)
 
 class RemoteSnapshot(CoreSnapshot):
     def _get_path(self):
-        dir_name = str(self.id)
-        if self.state != COMPLETE:
-            dir_name = '%s.%s' % (dir_name, self.state)
-        return '%s@%s%s' % (self.volume.ssh_user, self.volume.ssh_path,
-            os.sep + os.path.join(SNAPSHOT_DIR, dir_name) + os.sep)
+        return ''
 
     def _get_log_path(self):
-        return os.path.join(self.volume.log_dir, 'snapshot_%s.log' % self.id)
+        return ''
 
     def _setup_snapshot(self, last_snapshot):
         pass
