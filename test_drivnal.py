039a79b838f9f33c7b476f2f6660ab08bd005019
Zachary Huff <zach.huff.386@gmail.com>
2014-01-19 15:50:30 -0500

    Update unittest

--- test_drivnal.py
+++ test_drivnal.py
@@ -1,4 +1,3 @@
-from drivnal import database
 import threading
 import unittest
 import requests
@@ -6,66 +5,27 @@ import json
 import time
 import os
 
-URL = 'http://localhost:6500'
+BASE_URL = os.getenv('BASE_URL', 'http://localhost:6500')
 HEADERS = {
-    'Content-type': 'application/json',
     'Accept': 'application/json',
 }
-TEMP_DATABSE_PATH = 'drivnal_test.db'
 
-
-class Database(unittest.TestCase):
-    def setUp(self):
-        if os.path.isfile(TEMP_DATABSE_PATH):
-            os.remove(TEMP_DATABSE_PATH)
-        self._db = database.Database(TEMP_DATABSE_PATH)
-
-    def tearDown(self):
-        self._db.close()
-        os.remove(TEMP_DATABSE_PATH)
-
-    def _fill_column_family(self, num):
-        for i in xrange(5):
-            for x in xrange(5):
-                self._db.set('column_family_%s' % num, 'row_%s' % i,
-                    'column_%s' % x, 'value_%s' % x)
-
-        for i in xrange(5):
-            for x in xrange(5):
-                value = self._db.get('column_family_%s' % num,
-                    'row_%s' % i, 'column_%s' % x)
-                self.assertEqual(value, 'value_%s' % x)
-
-        for i in xrange(5):
-            for x in xrange(5):
-                self._db.remove('column_family_%s' % num,
-                    'row_%s' % i, 'column_%s' % x)
-
-        for i in xrange(5):
-            for x in xrange(5):
-                value = self._db.get('column_family_%s' % num,
-                    'row_%s' % i, 'column_%s' % x)
-                self.assertEqual(value, None)
-
-    def test_database(self):
-        for i in xrange(2):
-            threads = []
-            for x in xrange(300):
-                thread = threading.Thread(target=self._fill_column_family,
-                    args=(x,))
-                thread.start()
-                threads.append(thread)
-
-            for thread in threads:
-                thread.join()
+_request = requests.api.request
+def request(method, endpoint, **kwargs):
+    headers = {
+        'Accept': 'application/json',
+    }
+    if 'json' in kwargs and kwargs['json']:
+        headers['Content-Type'] = 'application/json'
+        kwargs['data'] = json.dumps(kwargs.pop('json'))
+    return _request(method, BASE_URL + endpoint, headers=headers,
+        verify=False, **kwargs)
+requests.api.request = request
 
 
 class Volume(unittest.TestCase):
     def test_vagrant_volume(self):
-        #######################################################################
-        # Get volume
-        #######################################################################
-        response = requests.get('%s/volume' % URL, headers=HEADERS)
+        response = requests.get('/volume')
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
@@ -81,10 +41,7 @@ class Volume(unittest.TestCase):
         self.assertTrue(isinstance(data[0]['percent_used'], float))
 
     def test_volume_get_post_put_delete(self):
-        #######################################################################
-        # Create volume
-        #######################################################################
-        data = {
+        response = requests.post('/volume', json={
             'name': 'Unittest Volume',
             'source_path': '/',
             'path': '/tmp',
@@ -92,18 +49,14 @@ class Volume(unittest.TestCase):
             'schedule': '3days',
             'min_free_space': 0.2,
             'bandwidth_limit': 0,
-        }
-        response = requests.post('%s/volume' % URL, headers=HEADERS,
-            data=json.dumps(data))
+        })
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, {})
 
-        #######################################################################
-        # Get volume
-        #######################################################################
-        response = requests.get('%s/volume' % URL, headers=HEADERS)
+
+        response = requests.get('/volume')
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
@@ -125,10 +78,8 @@ class Volume(unittest.TestCase):
         self.assertEqual(volume['bandwidth_limit'], 0)
         self.assertTrue(isinstance(volume['percent_used'], float))
 
-        #######################################################################
-        # Modify volume
-        #######################################################################
-        data = {
+
+        response = requests.put('/volume/%s' % volume_id, json={
             'name': 'Unittest Volume 2',
             'source_path': '/home',
             'path': '/media',
@@ -136,9 +87,7 @@ class Volume(unittest.TestCase):
             'schedule': '1day',
             'min_free_space': 0.4,
             'bandwidth_limit': 10240,
-        }
-        response = requests.put('%s/volume/%s' % (URL, volume_id),
-            headers=HEADERS, data=json.dumps(data))
+        })
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
@@ -147,10 +96,8 @@ class Volume(unittest.TestCase):
         # Wait for volume move task to finish
         time.sleep(1)
 
-        #######################################################################
-        # Get volume
-        #######################################################################
-        response = requests.get('%s/volume' % URL, headers=HEADERS)
+
+        response = requests.get('/volume')
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
@@ -172,11 +119,8 @@ class Volume(unittest.TestCase):
         self.assertEqual(volume['bandwidth_limit'], 10240)
         self.assertTrue(isinstance(volume['percent_used'], float))
 
-        #######################################################################
-        # Delete volume
-        #######################################################################
-        response = requests.delete('%s/volume/%s' % (URL, volume['id']),
-            headers=HEADERS)
+
+        response = requests.delete('/volume/%s' % volume['id'])
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
